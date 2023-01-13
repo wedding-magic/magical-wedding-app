e7c1532f49f61ae71775b9d03dbb6b873f6f1cbc
Daniel Steinbrook <danielsteinbrook@Daniels-MacBook-Pro.local>
2023-01-12 20:33:36 -0500

    startsBatch

--- website/src/server/controllers/tusHooksController.js
+++ website/src/server/controllers/tusHooksController.js
@@ -1,7 +1,7 @@
 const tusHooksController = {};
 const {Storage} = require('@google-cloud/storage');
 const storage = new Storage();
-const bucketName = 'test_input_bucket29';
+const bucketName = 'magical-wedding-bucket-1';
 
 tusHooksController.renameFile = (req, res, next) => {
     console.log('renameFile invoked');
@@ -14,13 +14,29 @@ tusHooksController.renameFile = (req, res, next) => {
     const currKey = req.body.Upload.Storage.Key;
     const newKey = req.body.Upload.MetaData.filename;
 
+    res.locals.job_id = newKey.substring(0,newKey.indexOf("_"));
+
+    console.log("res.locals.job_id",res.locals.job_id);
+
+    if (req.body.Upload.MetaData.filetype !== 'image/jpeg') {
+        storage.bucket(bucketName).file(currKey).delete().then(
+            () => {console.log("deleted");
+            return next();}
+        )
+        .catch(err => console.log(err))
+    }
+
+    else {
+
     storage.bucket(bucketName).file(currKey).move(newKey).then(
         () => {return next();}
     )
     .catch(err => console.log(err));
     }
+}
+
     else {
-        return next();
+        return res.sendStatus(200);
     }
 
 };
