5eb7f99345f16dbed125fadc5b0572dfe21a31ce
Tim <tim@Tims-MacBook-Pro.local>
2023-01-18 15:35:06 +0700

    full pipeline updates

--- stable_diffusion/task helpers/instance-template-create.sh
+++ stable_diffusion/task helpers/instance-template-create.sh
@@ -1,6 +1,6 @@
-gcloud compute instance-templates create stable-diffusion-test-instance-template-3 \
+gcloud compute instance-templates create stable-diffusion-instance-template-4 \
     --machine-type n1-standard-4 \
     --accelerator type=nvidia-tesla-t4,count=1 \
-    --image=stable-diffusion-test-image-1 \
+    --image=stable-diffusion-image-1 \
     --maintenance-policy TERMINATE \
     --restart-on-failure
