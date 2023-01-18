36066cc7419436548442e899efecfc1dffc15ade
Tim <tim@Tims-MacBook-Pro.local>
2023-01-18 19:46:30 +0700

    renamed (not important)

--- stable_diffusion/task helpers/instance-template-create.sh
+++ stable_diffusion/task helpers/instance-template-create.sh
@@ -1,6 +1,6 @@
 gcloud compute instance-templates create stable-diffusion-instance-template-4 \
     --machine-type n1-standard-4 \
     --accelerator type=nvidia-tesla-t4,count=1 \
-    --image=stable-diffusion-image-1 \
+    --image=stable-diffusion-image-2 \
     --maintenance-policy TERMINATE \
     --restart-on-failure
